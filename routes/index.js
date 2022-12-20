21f5a42c1d581df32c0eda0ca6bb9378b1987756
MithunAj <117289568+MithunAj@users.noreply.github.com>
2022-12-20 07:48:05 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ const homeController = require('../controller/home')
 
 router.get('/home',homeController.homePage)
 
+router.get('/',function(req,res){
+    return res.redirect('/home');
+})
+
 router.use('/projects',require('./project'));
 
 router.use('/issues',require('./issue'));
